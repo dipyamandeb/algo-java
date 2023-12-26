66c589af38d1ef80af3fb389868d631c44955c0b
dipyamandeb <dipyamandeb19@gmail.com>
2023-12-26 15:51:15 +0530

    Done

---
+++ 125.valid-palindrome.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=125 lang=java
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
+            }
+            while (r > l && !Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
