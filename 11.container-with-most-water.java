b021727eb7ae77410b94925c07e8b4bc9692a9a0
dipyamandeb <dipyamandeb19@gmail.com>
2024-01-28 10:48:33 +0530

    Done

---
+++ 11.container-with-most-water.java
@@ -0,0 +1,36 @@
+ /*
+ * @lc app=leetcode id=11 lang=java
+ *
+ * [11] Container With Most Water
+ */
+
+// @lc code=start
+class Solution {
+    public int maxArea(int[] height) {
+        // max area = lenght * breadth
+        // breadth os dist. between two vertical lines
+        // length will be the min of two vertical lines
+        // move the pointer from the smallest of the two vertical lines
+
+        int l = 0;
+        int r = height.length - 1;
+
+        int res = 0;
+
+        while (l < r) {
+            int area = (r - l) * Math.min(height[r], height[l]);
+            res = Math.max(res, area);
+
+            // move the ptr
+            if (height[l] < height[r]) {
+                l++;
+
+            } else {
+                r--;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
