c8755f57df542c229ffab1ab258bd0316d18009e
dipyamandeb <dipyamandeb19@gmail.com>
2024-01-31 19:02:45 +0530

    Done

---
+++ 704.binary-search.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=704 lang=java
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        // two ptr, binary search
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                r--;
+            } else {
+                l++;
+            }
+        }
+
+        return -1;
+    }
+}
+// @lc code=end
+
