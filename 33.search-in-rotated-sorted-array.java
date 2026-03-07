76a04d8d4f2c5633a3f1c9fed9b42f5a79cc87dc
dipyamandeb <dipyamandeb19@gmail.com>
2026-03-08 00:11:14 +0530

    Done

---
+++ 33.search-in-rotated-sorted-array.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=33 lang=java
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // left half sorted
+
+            if (nums[left] <= nums[mid]) {
+                if (nums[mid] > target && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
+            }
+            // right side sorted
+            else {
+                if (nums[mid] < target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
